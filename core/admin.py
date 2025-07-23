5aa7d078e0fbeb9ceae308be0eb90d246610040d
alexbainter <alexbainter@gmail.com>
2025-07-23 13:12:07 -0500

    Add Study model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Study
 
-# Register your models here.
+
+@register(Study)
+class StudyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'authors']
+    verbose_name_plural = 'studies'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+    @classmethod
+    def create(cls, created_by):
+        return cls(created_by=created_by)
+
+
+class Study(BaseModel):
+    class Meta:
+        verbose_name_plural = 'studies'
+
+    name = models.CharField(max_length=240)
+    authors = models.CharField(max_length=240)
