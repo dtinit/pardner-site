fd2df0cb92af9b2191d88b5d92c9357c7872a382
Arthur Borem <arthurborem@uchicago.edu>
2025-08-14 09:15:23 -0500

    Adds Service and Vertical data models and incorporates Strava as a service (#24)
    * Adds additional service and uses new data model
    
    * removed unused study method

--- core/internal/transfer_service_utils.py
+++ core/internal/transfer_service_utils.py
@@ -1,6 +1,6 @@
-from core.models import ServiceAccount
-from pardnersite.settings import TUMBLR_CLIENT_ID, TUMBLR_CLIENT_SECRET
-from pardner.services import TumblrTransferService
+from core.models import Service
+from pardnersite.settings import STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, TUMBLR_CLIENT_ID, TUMBLR_CLIENT_SECRET
+from pardner.services import TumblrTransferService, StravaTransferService
 from pardner.verticals import Vertical
 from urllib.parse import urljoin
 
@@ -11,10 +11,17 @@ def _get_redirect_url(service_account_name, host):
 
 def get_transfer_service(service_account_name, host):
     match service_account_name.lower():
-        case ServiceAccount.ServiceName.TUMBLR:
+        case Service.ServiceName.TUMBLR:
             return TumblrTransferService(
                 client_id=TUMBLR_CLIENT_ID,
                 client_secret=TUMBLR_CLIENT_SECRET,
+                redirect_uri=_get_redirect_url(service_account_name, host),
+                verticals={Vertical.FeedPost},
+            )
+        case Service.ServiceName.STRAVA:
+            return StravaTransferService(
+                client_id=STRAVA_CLIENT_ID,
+                client_secret=STRAVA_CLIENT_SECRET,
                 redirect_uri=_get_redirect_url(service_account_name, host),
                 verticals={Vertical.FeedPost},
             )
