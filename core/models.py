91757983f994318ec7d4385cd28d62cc3ac757b4
Arthur Borem <arthurborem@uchicago.edu>
2025-08-20 14:58:22 -0500

    Uses session in data model and views to keep track of past donations (#26)

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.sessions.models import Session
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -34,10 +36,39 @@ class Study(BaseModel):
     services = models.ManyToManyField(Service)
 
 
+class ServiceAccountManager(models.Manager):
+    def get_or_create_from_session(self, study_id, session_id, service_id):
+        potential_service_accounts = self.filter(
+            study__pk=study_id, session__pk=session_id, service__pk=service_id
+        )
+        if potential_service_accounts.count() == 0:
+            service_account = ServiceAccount(
+                study_id=study_id, session_id=session_id, service_id=service_id
+            )
+            service_account.save()
+            return service_account
+
+        return potential_service_accounts.last()
+
+
 class ServiceAccount(BaseModel):
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, blank=True, null=True
+    )
     service = models.ForeignKey(Service, on_delete=models.CASCADE, blank=True, null=True)
     study = models.ForeignKey(Study, on_delete=models.CASCADE)
     access_token = models.TextField(blank=True)
     refresh_token = models.TextField(blank=True)
     expires_at = models.DateTimeField(blank=True, null=True)
     state = models.TextField(blank=True, null=True, unique=True)
+    completed_donation_at = models.DateTimeField(blank=True, null=True)
+
+    objects = ServiceAccountManager()
+
+    @property
+    def has_completed_donation(self):
+        return (
+            self.completed_donation_at < timezone.now()
+            if self.completed_donation_at
+            else False
+        )
