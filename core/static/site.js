93f59a4613b874b2992421f33a08d2899b763701
Arthur Borem <arthurborem@uchicago.edu>
2025-09-05 16:31:54 -0500

    Makes donation buttons clickable in home page again

--- core/static/site.js
+++ core/static/site.js
@@ -51,41 +51,64 @@
       })
   }
 
-  const initializeModalTrigger = (triggerEl) => {
-     const modalUrl = triggerEl.getAttribute(MODAL_TRIGGER_ATTRIBUTE_NAME);
-      triggerEl.addEventListener('click', () => {
-        triggerModal(modalUrl) 
-      })
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const modalTriggers = document.querySelectorAll(`[${MODAL_TRIGGER_ATTRIBUTE_NAME}]`);
-    modalTriggers.forEach((triggerEl) => initializeModalTrigger(triggerEl));
+  const addModalTriggers = () => {
+    const modalTriggers = document.querySelectorAll(
+      `[${MODAL_TRIGGER_ATTRIBUTE_NAME}]`
+    );
+    modalTriggers.forEach((triggerEl) => {
+      const modalUrl = triggerEl.getAttribute(MODAL_TRIGGER_ATTRIBUTE_NAME);
+      triggerEl.addEventListener("click", () => {
+        triggerModal(modalUrl);
+      });
+    });
     window.pardnersite = Object.assign({}, window.pardnersite, {
-      triggerModal
+      triggerModal,
+    });
+  };
+
+  // Need an observer for the home page study elements since these get
+  // rendered after DOMContentLoaded gets fired.
+  const observer = new MutationObserver((mutationList, observer) => {
+    mutationList.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        addModalTriggers();
+      }
     });
   });
 
   const fetchAndRenderFilteredStudies = (serviceName) => {
-    const path = serviceName ? `/study_list_items?filtered_service_name=${encodeURIComponent(serviceName)}` : '/study_list_items';
+    const path = serviceName
+      ? `/study_list_items?filtered_service_name=${encodeURIComponent(
+          serviceName
+        )}`
+      : '/study_list_items';
     fetch(path)
-      .then(response => response.text())
-      .then(studyListItemsHTML => {
-        const studyListOuterElements = document.getElementsByClassName('study-list');
+      .then((response) => response.text())
+      .then((studyListItemsHTML) => {
+        const studyListOuterElements =
+          document.getElementsByClassName('study-list');
         for (const studyList of studyListOuterElements) {
+          observer.observe(studyList, { childList: true });
           studyList.innerHTML = studyListItemsHTML;
         }
       });
-  }
-  
+  };
+
   document.addEventListener('DOMContentLoaded', () => {
-    const selectHTMLElements = document.getElementsByClassName("homepage--studies-list-header-dropdown");
+    // original call to open modal when Data Donation button is clicked,
+    // in the case when the single-study page is accessed.
+    addModalTriggers();
+    const selectHTMLElements = document.getElementsByClassName(
+      'homepage--studies-list-header-dropdown'
+    );
     if (selectHTMLElements.length > 0) {
       const selectHTMLElement = selectHTMLElements.item(0);
       // first time loading
       fetchAndRenderFilteredStudies(selectHTMLElement.value);
       // future changes
-      selectHTMLElement.addEventListener('change', (event) => fetchAndRenderFilteredStudies(event.target.value));
+      selectHTMLElement.addEventListener('change', (event) =>
+        fetchAndRenderFilteredStudies(event.target.value)
+      );
     }
   });
 })();
